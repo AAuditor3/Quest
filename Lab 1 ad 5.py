5b7f9a839be1c5ed1eb7c00756559d3233340d00
Auditor <appleauditor3@gmail.com>
2023-11-24 20:02:54 +0100

    Done.

---
+++ Lab 1 ad 5.py
@@ -0,0 +1,19 @@
+x = input("Podaj pierwszą liczbę: ")
+y = input("Podaj drugą liczbę: ")
+z = input("Podaj trzecią liczbę: ")
+
+if x < y:
+    highNum = x
+    x = y
+    y = highNum
+if x < z:
+    highNum = x
+    x = z
+    z = highNum
+
+if y < z:
+    highNum = y
+    y = z
+    z = highNum
+
+print(x, y, z)

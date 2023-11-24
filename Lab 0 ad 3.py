b7b8b6f469d3f2a3f18987aba2e3d4489ba27147
Auditor <appleauditor3@gmail.com>
2023-11-24 17:13:17 +0100

    Done.

---
+++ Lab 0 ad 3.py
@@ -0,0 +1,6 @@
+
+a = float(input("Podaj bok a prostokąta "))
+b = float(input("Podaj bok b prostokąta "))
+p = a * b
+obw = 2 * a + 2 * b
+print("Pole prostokąta to " + str(p) + " a obwód prostokąta to " + str(obw))

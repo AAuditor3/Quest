be8be3d2d36a1477e1efe1022cfd809174d66cb0
Auditor <appleauditor3@gmail.com>
2023-12-10 17:09:57 +0100

    Done.

---
+++ Lab 2 ad 1.py
@@ -0,0 +1,14 @@
+a = int(input("Wprowadź pierwszą liczbę całkowitą "))
+b = int(input("Wprowadź drugą liczbę całkowitą "))
+if b > a:
+   highNum = b
+   lowNum = a
+elif a > b:
+   highNum = a
+   lowNum = b
+while lowNum < highNum:
+   lowNum = lowNum + 1
+   print(f"{lowNum} {highNum}")
+else:
+   print("Zrobione!")
+print(f"Liczby są równe {lowNum} {highNum}")
